529c99af3f9edcec5a08400aa2c7fd1400d3d76d
sumithra0900 <sumithra0907ar@gmail.com>
2022-09-27 23:34:44 +0530

    secomd commit

---
+++ src/main/java/maven/democlass2.java
@@ -0,0 +1,7 @@
+package maven;
+
+public class democlass2{
+	public String welcome() {
+		return "welcome";
+	}
+}
---
+++ src/test/java/maven/demotest2.java
@@ -0,0 +1,14 @@
+package maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demotest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
